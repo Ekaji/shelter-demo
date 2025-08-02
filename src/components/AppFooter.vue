3f416547f4c9ee476d7b5327fc25b94386d9112b
Ekaji Onah <ekajionah@gmail.com>
2025-08-02 12:39:04 +0100

    Update AppFooter.vue BG colour

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-gray-900 text-white pt-16 pb-8">
+  <footer class="bg-gray-50 text-white pt-16 pb-8">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
       <!-- Main Footer Content -->
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-12">
